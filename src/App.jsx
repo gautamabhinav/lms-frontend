6e0900710884734226ae0e8b787e20a07610a5cc
gautamabhinav <abhinavrajgautam01gmail.com>
2023-09-26 12:26:37 +0530

    Added denied page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom'
 import AboutUs from './Pages/AboutUs';
 import Contact from './Pages/Contact';
 import CourseList from './Pages/Course/CourseList';
+import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login'
 import NotFound from './Pages/NotFound';
@@ -23,6 +24,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/courses" element={<CourseList />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
 
 
         <Route path="*" element={<NotFound />}></Route>
